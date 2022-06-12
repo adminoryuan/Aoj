76fb4a29779249e45194429b17ef5bef49e4f043
adminoryuan <1317332365@qq.com>
2022-06-12 21:21:01 +0800

    add rank

--- api/src/main/java/com/practice/controller/RankController.java
+++ api/src/main/java/com/practice/controller/RankController.java
@@ -3,6 +3,8 @@ package com.practice.controller;
 import com.practice.common.Result;
 import com.practice.pojo.Vo.RankV0;
 import com.practice.service.RankService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,12 +16,14 @@ import java.util.List;
  * @date 2022/6/12 下午8:12
  */
 @RestController
+@Api(value = "Rank",description = "排行榜")
 public class RankController {
 
     @Autowired
     RankService rServer;
 
     @GetMapping("/Rank")
+    @ApiOperation("/Rank")
     public Result getRank(){
 
         return Result.ok(rServer.getRank());
