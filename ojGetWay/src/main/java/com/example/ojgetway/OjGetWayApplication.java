280c4da262873d792009a4682e58eb2577df97a2
adminoryuan <1317332365@qq.com>
2022-06-15 22:18:36 +0800

    add getway

--- ojGetWay/src/main/java/com/example/ojgetway/OjGetWayApplication.java
+++ ojGetWay/src/main/java/com/example/ojgetway/OjGetWayApplication.java
@@ -6,6 +6,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OjGetWayApplication {
 
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder){
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+
+        routes.route("Aoj_route_ojServer",
+                r -> r.path("/ojServer")
+                        .uri("http://127.0.0.1:8080")).build();
+
+        routes.route("Aoj_route_api",
+                r -> r.path("/api")
+                        .uri("http://127.0.0.1:9991")).build();
+        return routes.build();
+    }
     public static void main(String[] args) {
         SpringApplication.run(OjGetWayApplication.class, args);
     }
