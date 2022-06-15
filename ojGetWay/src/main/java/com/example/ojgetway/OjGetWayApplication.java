5eca9926b9b2495cd057f17d15b5e50c45027a03
adminoryuan <1317332365@qq.com>
2022-06-15 22:14:24 +0800

    add getway moudul

---
+++ ojGetWay/src/main/java/com/example/ojgetway/OjGetWayApplication.java
@@ -0,0 +1,13 @@
+package com.example.ojgetway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OjGetWayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OjGetWayApplication.class, args);
+    }
+
+}
---
+++ ojGetWay/src/test/java/com/example/ojgetway/OjGetWayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ojgetway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OjGetWayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
