be13661f2da6011e8a9a361976c46274da9fe1ee
adminoryuan <1317332365@qq.com>
2022-06-16 12:29:14 +0800

    add Gateway Route

--- ojGetWay/src/main/java/com/example/ojgetway/OjGetWayApplication.java
+++ ojGetWay/src/main/java/com/example/ojgetway/OjGetWayApplication.java
@@ -2,8 +2,13 @@ package com.example.ojgetway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class OjGetWayApplication {
 
     @Bean
@@ -12,10 +17,12 @@ public class OjGetWayApplication {
 
         routes.route("Aoj_route_ojServer",
                 r -> r.path("/ojServer")
+                        .filters(o->o.stripPrefix(1))
                         .uri("http://127.0.0.1:8080")).build();
 
         routes.route("Aoj_route_api",
-                r -> r.path("/api")
+                r -> r.path("/api/**")
+                        .filters(f -> f.stripPrefix(1))
                         .uri("http://127.0.0.1:9991")).build();
         return routes.build();
     }
